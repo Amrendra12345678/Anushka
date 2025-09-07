fe5d20d9eff024e24111395f260c03087aeeaf74
Amrendra12345678 <amrendrarajput1@gmail.com>
2025-09-07 11:00:44 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Webhook server chal raha hai!"
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print("TradingView se data aaya:", data)
+    return {"status": "ok"}, 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
